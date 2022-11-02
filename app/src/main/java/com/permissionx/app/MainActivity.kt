7481bc8f2a8f42bcfed642f3114e8060bfca6181
linyao <1655827624@qq.com>
2022-11-02 08:16:23 +0800

    完成Permission库的开发与测试

--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.permissionx.app
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.permissionx.linyaodev.PermissionX
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        but.setOnClickListener {
+            PermissionX.request(this, android.Manifest.permission.CALL_PHONE){ allGranted, deniedList ->
+                if (allGranted){
+                    call()
+                }else{
+                    Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        but1.setOnClickListener {
+            PermissionX.request(this,
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_CONTACTS){ allGranted, deniedList ->
+                if (allGranted){
+                    call()
+                }else{
+                    Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
+    private fun call(){
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:1008611")
+            startActivity(intent)
+
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
     }
 }
