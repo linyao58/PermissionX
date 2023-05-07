843e3d8cae5131e2ea986326ab7c7aa82bae99b3
linyao <1655827624@qq.com>
2023-05-08 04:48:42 +0800

    修改

--- app/src/main/java/com/permissionx/app/MainNewActivity3.kt
+++ app/src/main/java/com/permissionx/app/MainNewActivity3.kt
@@ -1,6 +1,7 @@
 package com.permissionx.app
 
 import android.os.Bundle
+import android.util.Log
 import com.permissionx.linyaodev.NewLoginNewActivity
 
 
@@ -8,7 +9,7 @@ class MainNewActivity3 : NewLoginNewActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        Log.e("TAG", "onCreate: ")
 
     }
 }
