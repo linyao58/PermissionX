1050cc0751d364dc8b297f929d97dade6ae5393b
linyao <1655827624@qq.com>
2023-05-08 16:23:39 +0800

    改变位置

--- library/src/main/java/com/permissionx/linyaodev/LoginNewActivity.kt
+++ library/src/main/java/com/permissionx/linyaodev/LoginNewActivity.kt
@@ -7,16 +7,16 @@ import android.widget.TextView
 
 open class LoginNewActivity : BaseNewActivity() {
 
+    var login = findViewById<TextView>(R.id.login)
+    var number = findViewById<TextView>(R.id.number)
+    var e_number = findViewById<EditText>(R.id.e_number)
+    var password = findViewById<TextView>(R.id.password)
+    var p_number = findViewById<EditText>(R.id.p_number)
+    var btu = findViewById<Button>(R.id.btu)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        var login = findViewById<TextView>(R.id.login)
-        var number = findViewById<TextView>(R.id.number)
-        var e_number = findViewById<EditText>(R.id.e_number)
-        var password = findViewById<TextView>(R.id.password)
-        var p_number = findViewById<EditText>(R.id.p_number)
-        var btu = findViewById<Button>(R.id.btu)
 
     }
 }
